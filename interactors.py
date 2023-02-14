4270f7e7516d00c5911ddbc02cb2ae6b4f6fb558
Gerard Bentley <gerardrbentley@gmail.com>
2023-02-13 17:55:57 -0800

    feat: v1 viewer

---
+++ interactors.py
@@ -0,0 +1,193 @@
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import Dict, Tuple
+
+import httpx
+import toml
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="sea_reverse")
+creds = toml.load(".streamlit/secrets.toml")
+pocketbase_url = creds.get("pocketbase_url")
+pocketbase_username = creds.get("pocketbase_username")
+pocketbase_password = creds.get("pocketbase_password")
+google_place_api_key = creds.get("google_places_api_key")
+
+
+@dataclass
+class Place:
+    record_id: str
+    name: str
+    description: str
+    category: str
+    tags: str
+    cost_per_person: int
+    homepage: str
+    maps_url: str
+    lat: float
+    lon: float
+    rating: float
+
+
+@lru_cache(1)
+def get_token() -> str:
+    response = httpx.post(
+        f"{pocketbase_url}/api/collections/users/auth-with-password",
+        json={
+            "identity": pocketbase_username,
+            "password": pocketbase_password,
+        },
+    )
+    try:
+        response.raise_for_status()
+        auth_data = response.json()
+        token = auth_data.get("token")
+    except Exception as e:
+        print(repr(e))
+        return ""
+    return token
+
+
+def get_mappings(table_name):
+    token = get_token()
+    if token == "":
+        print("Unable to get token")
+        return None
+
+    response = httpx.get(
+        f"{pocketbase_url}/api/collections/{table_name}/records",
+        headers={"Authorization": token},
+    )
+    response.raise_for_status()
+    data = response.json()["items"]
+    return {x["name"]: x["id"] for x in data}
+
+
+def update_place(record, record_id):
+    token = get_token()
+    if token == "":
+        print("Unable to get token")
+        return None
+
+    response = httpx.patch(
+        f"{pocketbase_url}/api/collections/places/records/{record_id}",
+        headers={"Authorization": token},
+        json=record,
+    )
+    return response
+
+
+def add_place(record):
+    token = get_token()
+    if token == "":
+        print("Unable to get token")
+        return None
+
+    response = httpx.post(
+        f"{pocketbase_url}/api/collections/places/records",
+        headers={"Authorization": token},
+        json=record,
+    )
+    return response
+
+
+def get_data() -> Tuple[Dict[str, Place], Dict[str, str], Dict[str, str]]:
+    token = get_token()
+    if token == "":
+        print("Unable to get token")
+        return {}, {}, {}
+
+    response = httpx.get(
+        f"{pocketbase_url}/api/collections/places/records",
+        headers={"Authorization": token},
+        params={
+            "perPage": 300,
+            "sort": "name",
+            "expand": ",".join(("category", "tags")),
+        },
+    )
+    try:
+        response.raise_for_status()
+        raw_data = response.json()
+        data = [parse_place(record) for record in raw_data["items"]]
+        mapping = {x.name: x for x in data}
+    except Exception as e:
+        print(repr(e))
+        return {}, {}, {}
+    try:
+        categories = get_mappings("categories")
+    except Exception as e:
+        print(repr(e))
+        return {}, {}, {}
+    try:
+        tags = get_mappings("tags")
+    except Exception as e:
+        print(repr(e))
+        return {}, {}, {}
+
+    return mapping, categories, tags
+
+
+def parse_place(raw: dict) -> Place:
+    place = Place(
+        record_id=raw.get("id", "Unknown"),
+        name=raw.get("name", "Unknown"),
+        cost_per_person=raw.get("cost_per_person", 0),
+        homepage=raw.get("homepage", "Unknown"),
+        maps_url=raw.get("maps_url", "Unknown"),
+        lat=raw.get("lat", 0),
+        lon=raw.get("lon", 0),
+        description=raw.get("description", "Unknown"),
+        category="Unknown",
+        tags="",
+        rating=raw.get("rating", 0),
+    )
+
+    joined_data = raw.get("expand")
+    if joined_data is not None:
+        category = joined_data.get("category")
+        if category is not None:
+            place.category = category.get("name", "Unknown")
+
+        tags = joined_data.get("tags")
+        if tags is not None:
+            tag_names = {tag.get("name", "Unknown") for tag in tags}
+            place.tags = ",".join(tag_names)
+
+    return place
+
+
+def lookup_place(name: str):
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+
+    response = httpx.get(
+        url,
+        params={
+            "input": name,
+            "inputtype": "textquery",
+            "locationbias": "circle:10000@47.6074883,-122.3293073",
+            "fields": "place_id",
+            # "fields": "name,rating,geometry,url",
+            "key": google_place_api_key,
+        },
+    )
+
+    response.raise_for_status()
+    print(response.request.url)
+    candidates = response.json().get("candidates")
+    if candidates is None:
+        raise Exception("Place Not Found")
+    place_id = candidates[0].get("place_id")
+    url = "https://maps.googleapis.com/maps/api/place/details/json"
+
+    response = httpx.get(
+        url,
+        params={
+            "place_id": place_id,
+            # "fields": "name,rating,geometry,url",
+            "key": google_place_api_key,
+        },
+    )
+
+    response.raise_for_status()
+    return response.json()
---
+++ streamlit_app.py
@@ -0,0 +1,169 @@
+from typing import List, Set, Union
+
+import folium
+import pandas as pd
+import streamlit as st
+from streamlit_folium import st_folium
+
+from interactors import Place, get_data
+
+get_data = st.cache_data()(get_data)
+SELECTED_LOCATION = "selected_location"
+CENTER_START = [47.6142, -122.3211]
+ZOOM_START = 16
+
+st.set_page_config("Seattle Places", layout="wide")
+
+st.header("Seattle Places")
+
+data, categories, tags = get_data()
+if not len(data):
+    st.exception("Error Getting Data")
+    st.stop()
+
+
+@st.cache_data
+def filter_data(
+    raw_data: List[Place],
+    selected_categories: Set[str],
+    selected_tags: Set[str],
+    min_price: Union[float, None],
+    max_price: Union[float, None],
+    min_rating: Union[float, None],
+) -> pd.DataFrame:
+    good_data = []
+    for row in raw_data:
+        if row.category not in selected_categories:
+            continue
+        if len(selected_tags) and not selected_tags.intersection(
+            set(row.tags.split(","))
+        ):
+            continue
+        if min_price is not None and row.cost_per_person < min_price:
+            continue
+        if max_price is not None and row.cost_per_person > max_price:
+            continue
+        if min_rating is not None and row.rating < min_rating:
+            continue
+        good_data.append(row)
+    return pd.DataFrame(good_data).drop("record_id", axis=1)
+
+
+with st.expander("Filter Locations: ", True):
+    selected_filters = st.multiselect(
+        "Filter Options",
+        ["Categories", "Tags", "Min Price", "Max Price", "Min Rating"],
+        ["Categories"],
+    )
+    if "Categories" in selected_filters:
+        selected_categories = st.multiselect("Categories", categories, categories)
+    else:
+        selected_categories = set()
+    if "Tags" in selected_filters:
+        selected_tags = st.multiselect("Tags", tags, None)
+    else:
+        selected_tags = set()
+    if "Min Price" in selected_filters:
+        min_price = st.number_input("Min Price ($)", 0)
+    else:
+        min_price = None
+    if "Max Price" in selected_filters:
+        max_price = st.number_input("Max Price ($)", 0)
+    else:
+        max_price = None
+    if "Min Rating" in selected_filters:
+        min_rating = st.number_input("Min Rating (*)", 0, 5)
+    else:
+        min_rating = None
+
+    filtered_data = filter_data(
+        list(data.values()),
+        set(selected_categories),
+        set(selected_tags),
+        min_price,
+        max_price,
+        min_rating,
+    )
+    st.dataframe(filtered_data)
+
+
+left_col, right_col = st.columns(2)
+
+fg = folium.FeatureGroup(name="Seattle Locations")
+m = folium.Map(location=CENTER_START, zoom_start=ZOOM_START)
+
+# https://fontawesome.com/v4/icons/
+icon_map = {
+    "Restaurant": "cutlery",
+    "Bar": "glass",
+    "Nature": "tree",
+    "Coffee Shop": "coffee",
+    "Shopping": "shopping-bag",
+    "Learning": "book",
+}
+
+# [‘red’, ‘blue’, ‘green’, ‘purple’, ‘orange’, ‘darkred’,
+# ’lightred’, ‘beige’, ‘darkblue’, ‘darkgreen’, ‘cadetblue’, ‘darkpurple’, ‘white’, ‘pink’, ‘lightblue’, ‘lightgreen’, ‘gray’, ‘black’, ‘lightgray’]
+color_map = {
+    "Restaurant": "blue",
+    "Bar": "red",
+    "Nature": "darkgreen",
+    "Coffee Shop": "beige",
+    "Shopping": "purple",
+    "Learning": "gray",
+}
+
+for row in filtered_data.itertuples():
+    color = color_map.get(row.category)
+    if row.name == st.session_state.get(SELECTED_LOCATION):
+        color = "black"
+    fg.add_child(
+        folium.Marker(
+            [row.lat, row.lon],
+            popup=[row.name, row.category],
+            tooltip=row.name,
+            icon=folium.Icon(color=color, icon=icon_map.get(row.category), prefix="fa"),
+        )
+    )
+
+
+with left_col:
+    map_selection = st_folium(
+        m,
+        width=725,
+        returned_objects=[
+            "last_object_clicked",
+            "last_object_clicked_tooltip",
+            "last_clicked",
+        ],
+        feature_group_to_add=fg,
+    )
+
+
+if (
+    map_selection.get("last_object_clicked_tooltip") is None
+    and st.session_state.get(SELECTED_LOCATION) is None
+):
+    st.warning("Select a location to view details")
+    st.stop()
+
+if (
+    st.session_state.get(SELECTED_LOCATION)
+    != map_selection["last_object_clicked_tooltip"]
+):
+    st.session_state[SELECTED_LOCATION] = map_selection["last_object_clicked_tooltip"]
+    st.experimental_rerun()
+
+lookup = data[st.session_state[SELECTED_LOCATION]]
+
+with right_col:
+    st.header(lookup.name)
+    st.write("## Category: " + lookup.category)
+    st.write("## Tags: ", lookup.tags)
+    st.write("## Description:\n\n" + lookup.description.replace("\n", "\n\n"))
+
+    st.write("## Cost Per Person: " + f"$`{lookup.cost_per_person:.2f}`")
+    st.write(f"## Rating: `{lookup.rating}`")
+    st.write(f"## Homepage: [{lookup.homepage}]()")
+    st.write(f"## Maps URL: [{lookup.maps_url}]()")
+    st.write(f"## Location: `{lookup.lat},{lookup.lon}`")
